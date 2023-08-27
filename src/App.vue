dd95398ed4edae56f233a4ccf8882703bb48260b
Anton Platonov <plantik@gmail.com>
2023-08-27 23:55:04 +0600

    setup:  structured folders, added dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
-
-    <div class="">
-      <div> Good</div>
+  <header class="flex place-content-center w-full bg-gray-200">
+    <div>
+      <div
+        class="text-5xl font-bold text-slate-900 textslate-800 w-fit rounded px-10 py-2 m-3 bg-gray-200"
+      >
+        Simple Page Builder Demo
+      </div>
     </div>
   </header>
 
   <main>
-    <div> Hello</div>
+    <PageEditor />
   </main>
 </template>
+
+<script setup>
+import PageEditor from '@/layout/PageEditor.vue'
+</script>
 
 
---
+++ src/layout/PageEditor.vue
@@ -0,0 +1,13 @@
+<template>
+    <div >
+        Hi, planet
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
