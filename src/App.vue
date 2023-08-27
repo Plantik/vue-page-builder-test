fe2584d42c536153f46c49d009bb97ac18f376e7
Anton Platonov <plantik@gmail.com>
2023-08-27 23:25:30 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+    <div class="">
+      <div> Good</div>
+    </div>
+  </header>
+
+  <main>
+    <div> Hello</div>
+  </main>
+</template>
+
+
